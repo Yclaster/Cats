770da1594e9e2e2af32f3ad75766a7b738ae4b7e
YU <juliareis2005paz@gmail.com>
2024-09-07 03:13:23 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -4,7 +4,7 @@ let dados = [
         titulo: "Maine Coon",
         descricao: "O Maine Coon é uma das maiores raças de gatos domésticos, famoso por seu porte grande e personalidade amigável. Apesar de seu tamanho imponente, o Maine Coon tem uma 'voz' suave e melodiosa, sendo um companheiro muito leal.",
         link: "https://pt.wikipedia.org/wiki/Maine_Coon",
-        tags: "gato grande, gato gigante, mane con"
+        tags: "gato grande, mane con, porte grande, gatao, gatão"
 
     },
     {
@@ -24,6 +24,30 @@ let dados = [
         descricao: "O Bombaim (gato preto) é extremamente afetuoso e apegado aos donos, gosta de seguir pessoas pela casa e é muito sociável. Apesar da aparência selvagem, ele é calmo e adora ambientes familiares tranquilos.",
         link: "https://pt.wikipedia.org/wiki/Bombaim_(gato)",
         tags: "gato preto, olho amarelo, boombaim, gato da sorte, azar"
+    },
+    {
+    titulo: "Persa",
+    descricao: "Conhecido por sua pelagem longa e sedosa, o Persa é um gato calmo e elegante. Seus olhos grandes e redondos dão-lhe um ar de doçura.",
+    link: "https://pt.wikipedia.org/wiki/Gato_persa",
+    tags: "gato persa, pelo longo, olhos grandes, gato calmo"
+    },
+    {
+    titulo: "Sphynx",
+    descricao: "O Sphynx é uma raça de gato sem pelo, conhecido por sua pele macia e quente. Sua aparência única e personalidade curiosa o tornam bastante popular.",
+    link: "https://pt.wikipedia.org/wiki/Sphynx",
+    tags: "gato sem pelo, sphynx, gato pelado"
+    },
+    {
+    titulo: "Ragdoll",
+    descricao: "O Ragdoll é um gato de grande porte, conhecido por sua pelagem macia e sua natureza dócil. Ele relaxa completamente quando é pego no colo, como um boneco de pano.",
+    link: "https://pt.wikipedia.org/wiki/Ragdoll",
+    tags: "ragdoll, gato gigante, gato dócil"
+    },
+    {
+    titulo: "Siberiano",
+    descricao: "Originário da Sibéria, este gato possui uma pelagem densa e impermeável que o protege do frio. É conhecido por sua natureza independente e brincalhona.",
+    link: "https://pt.wikipedia.org/wiki/Gato_siberiano",
+    tags: "siberiano, gato peludo, gato resistente"
     },
 ];
 
