3541aecdff12004e1cf7fed2bcc27759e25b8d7d
YU <juliareis2005paz@gmail.com>
2024-09-07 01:34:51 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    
+    {
+        titulo: "Maine Coon",
+        descricao: "O Maine Coon é uma das maiores raças de gatos domésticos, famoso por seu porte grande e personalidade amigável. Apesar de seu tamanho imponente, o Maine Coon tem uma 'voz' suave e melodiosa, sendo um companheiro muito leal.",
+        link: "https://pt.wikipedia.org/wiki/Maine_Coon",
+        tags: "gato grande, gato gigante, mane con"
+
+    },
+    {
+        titulo: "Siamês",
+        descricao: "O Siamês é conhecido por seu corpo elegante, olhos azuis e personalidade vocal. Os gatos siameses são incrivelmente vocais, muitas vezes 'conversando' com seus donos usando uma variedade de miados.",
+        link: "https://pt.wikipedia.org/wiki/Siam%C3%AAs_(gato)",
+        tags: "siames, gato siames, gato de olho azul"
+    },
+    {
+        titulo: "Frajola",
+        descricao: "O Frajola, ou 'Tuxedo Cat', não é uma raça específica, mas sim um padrão de pelagem preta e branca. Esses gatos são conhecidos por serem brincalhões e sociáveis. São frequentemente associados ao famoso personagem dos desenhos animados.",
+        link: "https://pt.wikipedia.org/wiki/Gato_bicolor",
+        tags: "frajolinha, gato bicolor, gato preto e branco"
+    },
+    {
+        titulo: "Bombaim",
+        descricao: "O Bombaim (gato preto) é extremamente afetuoso e apegado aos donos, gosta de seguir pessoas pela casa e é muito sociável. Apesar da aparência selvagem, ele é calmo e adora ambientes familiares tranquilos.",
+        link: "https://pt.wikipedia.org/wiki/Bombaim_(gato)",
+        tags: "gato preto, olho amarelo, boombaim, gato da sorte, azar"
+    },
+];
+
